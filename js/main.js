0621c8772c9aa928d207bb463de905717deb9ce1
Alexey <alexey.anasov@yandex.by>
2024-04-04 20:31:52 +0300

    fix about слайдера + верстка и стилизация секций  аккордеона и продуктов

--- js/main.js
+++ js/main.js
@@ -13,10 +13,16 @@ const swiperTop = new Swiper(".top__swiper", {
 
 const swiperAbout = new Swiper(".about__slider", {
   slidesPerView: 4,
-  spaceBetween: 30,
+  spaceBetween: 20,
   freeMode: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+document.querySelectorAll(".accordeon__triger").forEach((item) => {
+  item.addEventListener("click", () => {
+    item.parentNode.classList.toggle("accordeon__item--active");
+  });
 });
